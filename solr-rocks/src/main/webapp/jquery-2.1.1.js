cb144fcf36ce76224fe6d9be1ef7ddd01b20bad2
Mark Jenkins <mark@parit.ca>
2014-09-08 21:00:58 -0500

    licenses better documented towards librejs compatibility

--- solr-rocks/src/main/webapp/jquery-2.1.1.js
+++ solr-rocks/src/main/webapp/jquery-2.1.1.js
@@ -1,3 +1,5 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
+
 /*!
  * jQuery JavaScript Library v2.1.1
  * http://jquery.com/
@@ -9188,3 +9190,4 @@ if ( typeof noGlobal === strundefined ) {
 return jQuery;
 
 }));
+// @license-end

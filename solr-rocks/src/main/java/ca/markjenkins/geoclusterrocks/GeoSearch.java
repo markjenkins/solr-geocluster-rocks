3388d5b34b0876d7eaee6326763036bba2aaeeab
Mark Jenkins <mark@parit.ca>
2014-09-11 14:17:52 -0500

    Now returns facet based clusters

--- solr-rocks/src/main/java/ca/markjenkins/geoclusterrocks/GeoSearch.java
+++ solr-rocks/src/main/java/ca/markjenkins/geoclusterrocks/GeoSearch.java
@@ -24,6 +24,13 @@ import org.apache.solr.common.util.NamedList;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrDocument;
 
+import com.github.davidmoten.geo.GeoHash;
+import com.github.davidmoten.geo.LatLong;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class GeoSearch extends WebPage {
     static final SolrServer solr;
     static {
@@ -32,36 +39,82 @@ public class GeoSearch extends WebPage {
 
     static Logger log_l4 = LoggerFactory.getLogger( GeoSearch.class );
 
-    public GeoSearch(PageParameters pageParameters) {
-	RequestCycle cy = getRequestCycle();
-
-	QueryResponse rsp;
+    public static QueryResponse query_locations_in_solr
+	(String bounds, Set facet_queries ){
+	QueryResponse rsp = null;
 	SolrQuery params = new SolrQuery();
-	String[] queryBounds =
-	    cy.getRequest().getQueryParameters().getParameterValue("bounds")
-	    .toString().split(",");
+	String[] queryBounds = bounds.split(",");
+	String bot_left_long = queryBounds[0];
+	String bot_left_lat = queryBounds[1];
+	String top_right_long = queryBounds[2];
+	String top_right_lat = queryBounds[3];
+
 	params.setQuery("location:[" +
-			queryBounds[1] + "," +
-			queryBounds[0] +
+			bot_left_lat + "," +
+			bot_left_long +
 			" TO " + 
-			queryBounds[3] + "," +
-			queryBounds[2] + "]");
+			top_right_lat + "," +
+			top_right_long + "]");
 	params.setFacet(true);
 	params.addFacetField("location");
-	params.setFacetLimit(200);
+	params.setFacetLimit(0);
+	// fortunately going from a bottom left to top right system to a
+	// top left to bottom right system only requires mental gymanstics
+	Set<String> prefix_hashes = GeoHash.coverBoundingBoxMaxHashes
+	    (Double.parseDouble(top_right_lat), // topLeftLat
+	     Double.parseDouble(bot_left_long), // topLeftLon
+	     Double.parseDouble(bot_left_lat), // bottomRightLat
+	     Double.parseDouble(top_right_long), // bottomRightLon
+	     100).getHashes();
+	for (String geohash_prefix: prefix_hashes){
+	    String q_string = "{!prefix f=location}" + geohash_prefix;
+	    params.addFacetQuery(q_string);
+	    facet_queries.add(geohash_prefix);
+	}
 
 	try {
 	    rsp = solr.query( params );
 	}
 	catch (SolrServerException ex) {
 	    log_l4.warn( "unable to execute query", ex );
-	    return;
+	    return rsp;
 	}
 
+	return rsp;
+    }
+
+    public GeoSearch(PageParameters pageParameters) {
+	RequestCycle cy = getRequestCycle();
+
+	Set<String> facet_queries = new HashSet();
+	QueryResponse rsp = query_locations_in_solr(
+	    cy.getRequest().getQueryParameters().getParameterValue("bounds")
+	    .toString(),
+	    facet_queries );
+
+	if (rsp == null)
+	    return;
+	
 	FeatureCollection fc = new FeatureCollection();
 
 	NamedList<Object> solr_response = rsp.getResponse();
-	for (SolrDocument doc:
+
+	for (String facet_query: facet_queries){
+	    int count = (Integer)solr_response.findRecursive
+		("facet_counts", "facet_queries",
+		 "{!prefix f=location}" + facet_query );
+	    if ( count > 1 ){
+	    Feature f = new Feature();
+	    f.setProperty("clusterCount", count);
+	    LatLong lat_long = GeoHash.decodeHash(facet_query);
+	    f.setGeometry(new Point( lat_long.getLon(),
+				     lat_long.getLat()
+				     ) );
+	    fc.add(f);
+	    }
+	}
+
+	for (SolrDocument doc: 
 		 (SolrDocumentList) solr_response.get("response") ){
 	    Feature f = new Feature();
 	    f.setProperty("popupContent",

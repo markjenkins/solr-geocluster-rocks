ab40e032e350fb3d741357551ef4dddf4bb2758e
Mark Jenkins <mark@parit.ca>
2014-10-20 19:55:55 -0500

    latitude and longitude added as seprate fields

--- solr-rocks/data/to_xml.py
+++ solr-rocks/data/to_xml.py
@@ -9,11 +9,16 @@ with open('US.txt') as f:
     for i, line in enumerate(f,1):
         line_splits = line.split('\t')
         name_of_location = line_splits[1]
+        latitude, longitude = line_splits[4:5+1]
         print """<doc>
 <field name="id">%s</field>
 <field name="name">%s</field>
+<field name="latitude">%s</field>
+<field name="longitude">%s</field>
 <field name="location">%s, %s</field>
-</doc>""" % (line_splits[0], escape(name_of_location), line_splits[4], line_splits[5] )
+</doc>""" % (line_splits[0], escape(name_of_location),
+             latitude, longitude,
+             latitude, longitude )
         if len(argv) > 1 and i >= int(argv[1]):
             break
     print "</add>"

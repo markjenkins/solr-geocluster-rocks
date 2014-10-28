af03925d60055d86c741a09dbbb6a44bec62f91f
Mark Jenkins <mark@parit.ca>
2014-10-27 19:27:22 -0500

    Preparing to do some additional merge clustering with code from Drupal geocluster

--- solr-rocks/src/main/java/ca/markjenkins/geoclusterrocks/Clustering.java
+++ solr-rocks/src/main/java/ca/markjenkins/geoclusterrocks/Clustering.java
@@ -1,6 +1,9 @@
 package ca.markjenkins.geoclusterrocks;
 
 import com.spatial4j.core.io.GeohashUtils;
+import org.geojson.Point;
+import org.geojson.LngLatAlt;
+import org.apache.lucene.util.SloppyMath;
 
 public class Clustering {
     // this is pretty much strait lifted from
@@ -12,12 +15,17 @@ public class Clustering {
     //maxResolution = GEOFIELD_KILOMETERS * 1000 / $tile_size;
     public static final double MAX_RESOLUTION = 156.412 * 1000;
 
+    public static final double[] resolutions;
     public static final int[] geohash_lengths_for_zooms;
+    // as the following illustrates, a "final" array isn't so final...
+    // in some situations this can be a security problem
     static {
+	resolutions = new double[ZOOMS];
 	geohash_lengths_for_zooms = new int[ZOOMS];
 	for( int zoom=0; zoom < ZOOMS; zoom++ ){
+	    resolutions[zoom] = MAX_RESOLUTION / Math.pow(2, zoom);
 	    geohash_lengths_for_zooms[zoom] =
-		lengthFromDistance(MAX_RESOLUTION / Math.pow(2, zoom) );
+		lengthFromDistance(resolutions[zoom]);
 	}
     }
 
@@ -67,4 +75,49 @@ http://cgit.drupalcode.org/geocluster/tree/includes/GeohashHelper.inc
 	return hashLen +1;
     }
 
+    /**
+     * Dumb implementation to incorporate pixel variation with latitude
+     * on the mercator projection.
+     *
+     * @todo: use a valid implementation instead of the current guessing.
+     *
+     * Current implementation is based on the observation:
+     * lat = 0 => output is correct
+     * lat = 48 => output is 223 pixels distance instead of 335 in reality.
+     *
+     * derived from pixel_correction() from
+     http://cgit.drupalcode.org/geocluster/tree/includes/GeoclusterHelper.inc
+     */
+    public static double pixel_correction(double lat) {
+	return 1 + (335.0 / 223.271875276 - 1) * (Math.abs(lat) / 47.9899);
+    }
+
+    /**
+     * Calculate the distance between two given points in pixels.
+     *
+     * This depends on the resolution (zoom level) they are viewed in.
+     *
+     * derived from distance_pixels() from 
+http://cgit.drupalcode.org/geocluster/tree/includes/GeoclusterHelper.inc
+     */
+    public static double distance_pixels(Point a, Point b, double resolution){
+	LngLatAlt a_coords = a.getCoordinates();
+	LngLatAlt b_coords = b.getCoordinates();
+	double a_latitude = a_coords.getLatitude();
+	double distance = SloppyMath.haversin(a_latitude,
+					      a_coords.getLongitude(),
+					      b_coords.getLatitude(),
+					      b_coords.getLongitude() );
+	// instead of doing this correciton, I think there are more
+	// sophisticated algorithms we can use for this distance to
+	// pixel conversion that don't have this problem because we
+	// feed them latitude to begin with
+	return distance / resolution * pixel_correction(a_latitude);
+    }
+
+    public static boolean shouldCluster(Point a, Point b, double resolution) {
+	// Calculate distance.
+	return distance_pixels(a, b, resolution)
+	    <= GEOCLUSTER_DEFAULT_DISTANCE;
+    }
 }
